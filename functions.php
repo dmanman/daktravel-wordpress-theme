c17118b3745a93dc5551d4377379be803564dfa4
dmanman <415189@gmail.com>
2026-08-11 14:29:31 +0200

    Add core theme functions

---
+++ functions.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * D.A.K Travel theme functions.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+function daktravel_setup() {
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'custom-logo' );
+    add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption', 'style', 'script' ) );
+
+    register_nav_menus(
+        array(
+            'primary' => __( 'Primary Menu', 'daktravel' ),
+            'footer'  => __( 'Footer Menu', 'daktravel' ),
+        )
+    );
+}
+add_action( 'after_setup_theme', 'daktravel_setup' );
+
+function daktravel_enqueue_assets() {
+    wp_enqueue_style( 'daktravel-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', 'daktravel_enqueue_assets' );
+
+function daktravel_register_post_types() {
+    register_post_type(
+        'dak_case_study',
+        array(
+            'labels' => array(
+                'name'          => __( 'Case Studies', 'daktravel' ),
+                'singular_name' => __( 'Case Study', 'daktravel' ),
+            ),
+            'public'       => true,
+            'show_in_rest' => true,
+            'menu_icon'    => 'dashicons-clipboard',
+            'supports'     => array( 'title', 'editor', 'excerpt', 'thumbnail' ),
+            'rewrite'      => array( 'slug' => 'case-studies' ),
+        )
+    );
+}
+add_action( 'init', 'daktravel_register_post_types' );
+
+function daktravel_whatsapp_url( $message = '' ) {
+    $base = 'https://wa.me/27824406144';
+    return $message ? $base . '?text=' . rawurlencode( $message ) : $base;
+}
