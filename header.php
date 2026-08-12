92c81f959454000847b0ac79648a096bd4430be3
dmanman <415189@gmail.com>
2026-08-12 12:47:54 +0200

    Preconnect critical mobile resources

--- header.php
+++ header.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <?php if ( is_front_page() ) : ?>
+        <link rel="preconnect" href="https://images.pexels.com" crossorigin>
+    <?php endif; ?>
     <?php wp_head(); ?>
     <style id="dak-logo-size-adjustment">
         @media (min-width: 981px) {
