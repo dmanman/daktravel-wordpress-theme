26588d0276a8f27663d299edad0b8461a7e43355
dmanman <415189@gmail.com>
2026-08-13 19:53:18 +0200

    Render homepage LCP heading without webfont delay

---
+++ inc/home-lcp.php
@@ -0,0 +1,8 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+function daktravel_home_lcp_style() {
+    if ( is_front_page() ) {
+        echo '<style>.home .hero h1{font-family:Georgia,"Times New Roman",serif!important;}</style>';
+    }
+}
+add_action( 'wp_head', 'daktravel_home_lcp_style', 1 );
