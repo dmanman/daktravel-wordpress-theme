8f9762aaf1a258aab3a65c6bca2b4a109981a4b5
dmanman <415189@gmail.com>
2026-08-13 19:57:55 +0200

    Reserve homepage utility link width to prevent tiny mobile CLS

--- inc/home-lcp.php
+++ inc/home-lcp.php
@@ -2,7 +2,7 @@
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 function daktravel_home_lcp_style() {
     if ( is_front_page() ) {
-        echo '<style>.home .hero h1{font-family:Georgia,"Times New Roman",serif!important;}</style>';
+        echo '<style>.home .hero h1{font-family:Georgia,"Times New Roman",serif!important;}@media(max-width:680px){.home .utility-links{min-width:170px;justify-content:flex-end;}}</style>';
     }
 }
 add_action( 'wp_head', 'daktravel_home_lcp_style', 1 );
