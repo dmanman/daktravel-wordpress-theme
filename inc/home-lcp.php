bda02ac158e2c0abca9d13c0c70c363b0c205e80
dmanman <415189@gmail.com>
2026-08-13 20:11:48 +0200

    Add reciprocal hreflang for English regulatory pages

--- inc/home-lcp.php
+++ inc/home-lcp.php
@@ -6,3 +6,19 @@ function daktravel_home_lcp_style() {
     }
 }
 add_action( 'wp_head', 'daktravel_home_lcp_style', 1 );
+
+function daktravel_regulatory_english_hreflang() {
+    if ( is_page( 'israel-eta-il-entry-requirements' ) ) {
+        $en = home_url( '/israel-eta-il-entry-requirements/' );
+        $he = home_url( '/he/israel-eta-il-entry-requirements/' );
+    } elseif ( is_page( 'south-africa-traveller-declaration' ) ) {
+        $en = home_url( '/south-africa-traveller-declaration/' );
+        $he = home_url( '/he/south-africa-traveller-declaration/' );
+    } else {
+        return;
+    }
+    printf( "<link rel=\"alternate\" hreflang=\"en-ZA\" href=\"%s\">\n", esc_url( $en ) );
+    printf( "<link rel=\"alternate\" hreflang=\"he-IL\" href=\"%s\">\n", esc_url( $he ) );
+    printf( "<link rel=\"alternate\" hreflang=\"x-default\" href=\"%s\">\n", esc_url( $en ) );
+}
+add_action( 'wp_head', 'daktravel_regulatory_english_hreflang', 3 );
