256d8e5384284810a0cc0afe2a534fa2c46cdfff
dmanman <415189@gmail.com>
2026-08-13 20:03:54 +0200

    Route Hebrew regulatory pages to dedicated wrappers

--- inc/hebrew-info-template.php
+++ inc/hebrew-info-template.php
@@ -4,8 +4,12 @@ if ( ! defined( 'ABSPATH' ) ) { exit; }
 function daktravel_hebrew_info_template_override( $template ) {
     if ( ! function_exists( 'daktravel_hebrew_path_key' ) ) { return $template; }
     $key = daktravel_hebrew_path_key();
-    if ( ! in_array( $key, array( 'eta-il', 'sars' ), true ) ) { return $template; }
-    $native = get_template_directory() . '/templates/hebrew-info.php';
+    $map = array(
+        'eta-il' => 'hebrew-eta-il.php',
+        'sars'   => 'hebrew-sars.php',
+    );
+    if ( ! isset( $map[ $key ] ) ) { return $template; }
+    $native = get_template_directory() . '/templates/' . $map[ $key ];
     return file_exists( $native ) ? $native : $template;
 }
 add_filter( 'template_include', 'daktravel_hebrew_info_template_override', 22000 );
