6dc6b11a859585f79716502d03313a1f84a69f64
dmanman <415189@gmail.com>
2026-08-13 18:27:26 +0200

    Add Hebrew South Africa to Israel template override

---
+++ inc/hebrew-to-israel-template.php
@@ -0,0 +1,12 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+function daktravel_hebrew_to_israel_template_override( $template ) {
+    if ( ! function_exists( 'daktravel_hebrew_path_key' ) || 'to-israel' !== daktravel_hebrew_path_key() ) {
+        return $template;
+    }
+
+    $native = get_template_directory() . '/templates/hebrew-to-israel.php';
+    return file_exists( $native ) ? $native : $template;
+}
+add_filter( 'template_include', 'daktravel_hebrew_to_israel_template_override', 20000 );
